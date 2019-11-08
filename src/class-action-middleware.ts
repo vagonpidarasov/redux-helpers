6ae2360858b4faa9e1f2dcf6c442ff90b5e8eb7f
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-11-08 14:47:12 +0100

    copy everything from original action

--- src/class-action-middleware.ts
+++ src/class-action-middleware.ts
@@ -1,4 +1,7 @@
 import {Action} from './action.interface';
 export const classActionMiddleware = () => (next:(a:Action) => Action) => (action:Action) => {
-    next(Object.assign({}, {type: action.type, payload: action.payload}));
+    next(Object.assign({}, action, {
+        type: action.type,
+        payload: action.payload
+    }));
 };
